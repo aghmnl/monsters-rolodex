0c996af5d03bfe7e85d337bbb0890c35185405d3
Agus <agustin@italic.com>
2023-07-03 23:31:05 -0300

    Ahora optimizamos el filtrado con useEffect

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   // setea los estados
   const [monsters, setMonsters] = useState([]); // inicializa el estado en []
   const [searchField, setSearchField] = useState(""); // inicializa el estado en ""
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -15,14 +16,17 @@ const App = () => {
       .then((users) => setMonsters(users)); // OJO que acá el valor de users es distinto en memoria del que tenemos guardado, por venir de afuera.
   }, []); // useEffect se ejecuta la primera vez que se ejecuta App, y también cada vez que cambian los valores dentro de este array
 
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(
+      (monster) => monster.name.toLocaleLowerCase().includes(searchField) // includes es case sensitive!!
+    );
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
   const onSearchChange = (event) => {
     const searchField = event.target.value.toLocaleLowerCase();
     setSearchField(searchField);
   };
-
-  const filteredMonsters = monsters.filter(
-    (monster) => monster.name.toLocaleLowerCase().includes(searchField) // includes es case sensitive!!
-  );
 
   return (
     <div className="App">
